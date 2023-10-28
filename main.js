783ea0aef6cb1deb56d1b3dd9a1859f60c18c404
Darkice42 <cryan42@protonmail.com>
2023-10-27 19:47:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Uses openweathermap api to return weather temperature
 async function getWeatherInfo(latitude, longitude) {
-    const apiKey = '74931d3bf884baaeb59e569ec12f7a33';
+    const apiKey = 'YOUROPENWEATHERMAPAPIKEYHERE';
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
     let response = await fetch(apiUrl);
     let jsonData = await response.json();
@@ -10,9 +10,9 @@ async function getWeatherInfo(latitude, longitude) {
 
 // Updates date and weather fields on page load
 async function updateItems() {
-    // Probably shouldn't upload this anywhere...
-    const latitude = 44.986;
-    const longitude = -93.228;
+    // Add you own latitude and longitude to display weather information
+    const latitude = 0;
+    const longitude = 0;
     
     let dateString = new Date().toDateString();
     let tempData = await getWeatherInfo(latitude, longitude);
